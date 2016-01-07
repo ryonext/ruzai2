a9bef49d0f512a38e814c945482717627b5a989b
ryonext <ryonext.s@gmail.com>
2016-01-07 22:40:07 +0900

    Write basic logic.

--- lib/ruzai2.rb
+++ lib/ruzai2.rb
@@ -1,5 +1,22 @@
 require "ruzai2/version"
 
 module Ruzai2
-  # Your code goes here...
+  class RuzaiList
+    def ban!(user_id, hoge_id, fuga_id, expired_at: nil)
+      RuzaiList.create(
+        user_id: user_id,
+        hoge_id: hoge_id,
+        fuga_id: fuga_id,
+        expired_at: expired_at || Time.zone.now + 7.days
+      )
+    end
+
+    def banned?(user_id, hoge_id, fuga_id)
+      RuzaiList.where(
+        user_id: user_id,
+        hoge_id: hoge_id,
+        fuga_id: fuga_id,
+      ).where("expired_at < ?", Time.zone.now).exist?
+    end
+  end
 end
